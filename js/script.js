1a4ec2f738d69a6d7bf984d98d23385c4e7b083b
William Italia <williamitalia70@outlook.com>
2024-12-17 18:00:55 -0300

    section: preview; modal checked

--- js/script.js
+++ js/script.js
@@ -1,81 +1,82 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const toggleButton = document.querySelector('.navbar__mobile-menu-toggle'); // Adicionado ponto
-    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');   // Adicionado ponto
-    const icon = document.querySelector('#menu__icon');
-    const openVideo = document.querySelector('.preview__video-button');
-    const closeVideo = document.querySelector('.modal__close-button');
-    const modal = document.querySelector('.modal');
+  const toggleButton = document.querySelector('.navbar__mobile-menu-toggle'); // Adicionado ponto
+  const mobileMenu = document.querySelector('.navbar__mobile-menu-items');   // Adicionado ponto
+  const icon = document.querySelector('#menu__icon');
+  const openVideo = document.querySelector('.preview__video-button');
+  const closeVideo = document.querySelector('.modal__close-button');
+  const modal = document.querySelector('.modal');
+  const videoPlayer = document.getElementById('videoPlayer')
 
-    toggleButton.addEventListener('click', function () {
+  toggleButton.addEventListener('click', function () {
 
-        mobileMenu.classList.toggle('active');
-        icon.classList.add('fade-out');
+      mobileMenu.classList.toggle('active');
+      icon.classList.add('fade-out');
 
-        setTimeout(() => {
-         
-          if (mobileMenu.classList.contains('active')) {
-              icon.classList.remove('fa-bars'); 
-              icon.classList.add('fa-times');  
-          } else {
-              icon.classList.remove('fa-times'); 
-              icon.classList.add('fa-bars');     
-          }
+      setTimeout(() => {
+       
+        if (mobileMenu.classList.contains('active')) {
+            icon.classList.remove('fa-bars'); 
+            icon.classList.add('fa-times');  
+        } else {
+            icon.classList.remove('fa-times'); 
+            icon.classList.add('fa-bars');     
+        }
 
-      }, 300);
+    }, 300);
 
-    });
+  });
 
-    openVideo.addEventListener('click', function () {
-      modal.classList.add('active');
-      // modal.style.display = 'block';
-    });
+  openVideo.addEventListener('click', function () {
+    modal.classList.add('active');
+    // modal.style.display = 'block';
+  });
+  
+  closeVideo.addEventListener('click', function () {
+    modal.classList.remove('active');
+    videoPlayer.src = '';
+    videoPlayer.src = 'https://www.youtube.com/embed/MaNDBD2EzJo?si=WtUeAVgzjajXKq3l?enablejsapi=1';
+    // modal.style.display = 'none';
+  });
+
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      autoplay: {
+          delay: 5000,
+      },
     
-    closeVideo.addEventListener('click', function () {
-      modal.classList.remove('active');
-      // modal.style.display = 'none';
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
     });
-
-    const swiper = new Swiper('.swiper', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        autoplay: {
-            delay: 5000,
-        },
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-
-        effect: 'fade',
-        fadeEffect: {
-          crossFade: true
-        },
-      });
-
 
 });
 
 window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector('.navbar');
 
-    if (window.scrollY > 0) {
-        navbar.classList.add('navbar--scroll');
-    } else {
-        navbar.classList.remove('navbar--scroll');
-    }
+  if (window.scrollY > 0) {
+      navbar.classList.add('navbar--scroll');
+  } else {
+      navbar.classList.remove('navbar--scroll');
+  }
 });
-
